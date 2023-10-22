a0bc1862b995705f40eb043ea47e7642b5d9067b
Josiah Leon <sjosiahleon@gmail.com>
2023-10-22 09:35:40 -0700

    added calendar add links to calendar and instagram

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,6 +3,7 @@ import './navbar.scss';
 
 import {ReactComponent as Insta} from './../../assets/svgs/instagram.svg';
 import { ReactComponent as Mail } from "./../../assets/svgs/envelope.svg";
+import { ReactComponent as Calendar } from "../../assets/svgs/calendar.svg";
 
 // will need to change the title into a link to the homepage when that is needed
 
@@ -13,13 +14,14 @@ const Navbar = () => {
             <div className="navbar-reserve-space"/>
             <div className="navbar">
                 <div className="navbar-middlebar">
-                    <div>Coming to Vesbers?</div>
+                    <div className="navbar-leftside">Coming to Vesbers?</div>
                     <div className="navbar-title">
                         <b>OCG</b>CYA
                     </div>
                     <div className="navbar-socials">
-                        <Insta/>
-                        <Mail/>
+                        <a href="https://www.instagram.com/ocgracecya/"><Insta/></a>
+                        <a href="https://teamup.com/ks6hmadkxqrjc4ns29"><Calendar/></a>
+                        {/* <a href="mailto:"><Mail/></a> */}
                     </div>
                 </div>
                 
