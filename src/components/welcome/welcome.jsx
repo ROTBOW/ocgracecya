c7f3c02157641a3463c09ea66b9bbbe7cd639233
Josiah Leon <sjosiahleon@gmail.com>
2023-10-22 09:36:21 -0700

    added filler text and adjusted styles for welcome page

--- src/components/welcome/welcome.jsx
+++ src/components/welcome/welcome.jsx
@@ -8,7 +8,18 @@ const Welcome = () => {
         <div className="welcome-container">
             <div className="welcome-body">
                 <h2>Welcome to OCGrace!</h2>
-                We're a small but close group of friends at OCGrace Adventist Church
+                We're a small but close group of friends at OCGrace Adventist Church <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ac turpis egestas integer eget aliquet
+                nibh praesent tristique. Sed tempus urna et pharetra pharetra massa massa ultricies.
+                Elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Augue neque gravida in
+                fermentum et sollicitudin ac orci phasellus. Nulla pharetra diam sit amet nisl. Donec
+                pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Suspendisse faucibus
+                interdum posuere lorem ipsum dolor sit amet consectetur. Latin is my favorite filler text
+                Pharetra vel turpis nunc eget
+                lorem dolor sed viverra ipsum. Dolor morbi non arcu risus quis varius quam. Dolor sit
+                amet consectetur adipiscing elit pellentesque habitant. Sed elementum tempus egestas sed sed.
+                Feugiat nisl pretium fusce id velit ut. Diam vel quam elementum pulvinar etiam non.
             </div>
         </div>
     );
