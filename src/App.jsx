f7cdcb9d79ce9c45f49510df32d5434858b38535
Josiah Leon <sjosiahleon@gmail.com>
2023-10-22 10:27:15 -0700

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import './styles/App.scss';
 import Background from './components/background/background';
 import Navbar from './components/navbar/navbar';
 import Welcome from './components/welcome/welcome';
+import Footer from './components/footer/footer';
 
 const App = () => {
 
   return (
-    <div>
+    <div className='app-container'>
       <Background/>
       <Navbar/>
       <Routes>
         <Route path="home" element={<Welcome/>}/>
         <Route path="*" element={ <Navigate to="home" replace />}/>
       </Routes>
+      <Footer/>
     </div>
   )
 };
