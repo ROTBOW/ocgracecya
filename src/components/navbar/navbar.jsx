7a07c39e97d8cfed7894e8000d5b3caaacba8cf0
Josiah Leon <sjosiahleon@gmail.com>
2023-10-22 08:35:09 -0700

    added svgs for insta and email

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import './navbar.scss';
 
+import {ReactComponent as Insta} from './../../assets/svgs/instagram.svg';
+import { ReactComponent as Mail } from "./../../assets/svgs/envelope.svg";
+
+// will need to change the title into a link to the homepage when that is needed
 
 const Navbar = () => {
 
     return (
         <div className="navbar">
-            <div>
+            <div className="navbar-middlebar">
+                <div>Coming to Vesbers?</div>
                 <div className="navbar-title">
                     <b>OCG</b>CYA
                 </div>
+                <div className="navbar-socials">
+                    <Insta/>
+                    <Mail/>
+                </div>
             </div>
+            
             <div className="navbar-line"/>
         </div>
     )
